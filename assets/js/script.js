e8ea6a3284ebf6f02d2fcfd236780338184cd61d
Naman Adlakha <naman.adlakha3@gmail.com>
2022-04-26 14:49:21 +0530

    Fixed counter function

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,32 +66,32 @@ $(document).ready(function () {
   });
 
   //  Count Up
-  function counter() {
-    var oTop;
-    if ($('.count').length !== 0) {
-      oTop = $('.count').offset().top - window.innerHeight;
-    }
-    if ($(window).scrollTop() > oTop) {
-      $('.count').each(function () {
-        var $this = $(this),
-          countTo = $this.attr('data-count');
-        $({
-          countNum: $this.text()
-        }).animate({
-          countNum: countTo
-        }, {
-          duration: 1000,
-          easing: 'swing',
-          step: function () {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function () {
-            $this.text(this.countNum);
-          }
-        });
-      });
-    }
-  }
+  // function counter() {
+  //   var oTop;
+  //   if ($('.count').length !== 0) {
+  //     oTop = $('.count').offset().top - window.innerHeight;
+  //   }
+  //   if ($(window).scrollTop() > oTop) {
+  //     $('.count').each(function () {
+  //       var $this = $(this),
+  //         countTo = $this.attr('data-count');
+  //       $({
+  //         countNum: $this.text()
+  //       }).animate({
+  //         countNum: countTo
+  //       }, {
+  //         duration: 1000,
+  //         easing: 'swing',
+  //         step: function () {
+  //           $this.text(Math.floor(this.countNum));
+  //         },
+  //         complete: function () {
+  //           $this.text(this.countNum);
+  //         }
+  //       });
+  //     });
+  //   }
+  // }
   $(window).on('scroll', function () {
     counter();
   });
